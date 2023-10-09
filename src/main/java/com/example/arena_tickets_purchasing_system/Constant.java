6a7cadba2a00514ad713a13b30b25d1c6194b4bd
SanyaShbn <144464447+SanyaShbn@users.noreply.github.com>
2023-10-09 21:10:11 +0300

    Add files via upload

---
+++ src/main/java/com/example/arena_tickets_purchasing_system/Constant.java
@@ -0,0 +1,21 @@
+package com.example.arena_tickets_purchasing_system;
+
+public class Constant {
+    public static final String USERS_TABLE = "users";
+    public static final String MATCHES_TABLE = "matches";
+    public static final String ADMINS_TABLE = "admins";
+    public static final String ADMIN_TICKETS_TABLE = "tickets";
+    public static final String MATCH_ID = "idMatches";
+    public static final String MATCHES_DATE = "Match_Date";
+    public static final String MATCHES_TIME = "Match_Time";
+    public static final String OPP_TEAM = "Opp_Team";
+    public static final String MATCH_TYPE = "Match_Type";
+    public static final String TICKETS_AMOUNT = "Tickets_Amount";
+    public static final String USERS_ID = "idUsers";
+    public static final String USERS_LOGIN = "User_login";
+    public static final String USERS_PASSWORD = "User_password";
+    public static final String ADMINS_ID = "idAdmins";
+    public static final String ADMINS_LOGIN = "Admin_login";
+    public static final String ADMINS_PASSWORD = "Admin_password";
+
+}
