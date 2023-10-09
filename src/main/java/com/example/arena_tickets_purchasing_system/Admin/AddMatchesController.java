37baa665465c8d509434aae58fc50327cd9cc39c
SanyaShbn <144464447+SanyaShbn@users.noreply.github.com>
2023-10-09 22:08:07 +0300

    Add files via upload

---
+++ src/main/java/com/example/arena_tickets_purchasing_system/Admin/AddMatchesController.java
@@ -0,0 +1,108 @@
+package com.example.arena_tickets_purchasing_system.Admin;
+
+import com.example.arena_tickets_purchasing_system.DatabaseHandler;
+import javafx.animation.FadeTransition;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+
+public class AddMatchesController {
+
+    @FXML
+    private AnchorPane MainPane;
+
+    @FXML
+    private TextField amount;
+
+    @FXML
+    private TextField date;
+
+    @FXML
+    private Button exitButton;
+
+    @FXML
+    private RadioButton awayRadioButton;
+
+    @FXML
+    private RadioButton homeRadioButton;
+
+    @FXML
+    private TextField id;
+
+    @FXML
+    private TextField opponent;
+
+    @FXML
+    private Button submitChanges;
+
+    @FXML
+    private TextField time;
+
+
+    AnchorPane back_to_admin_matches;
+    @FXML
+    void initialize(){
+        homeRadioButton.setSelected(true);
+        FXMLLoader add_matches_loader = new FXMLLoader();
+        add_matches_loader.setLocation((getClass().getResource("admin_matches.fxml")));
+        try {
+            back_to_admin_matches = add_matches_loader.load();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @FXML
+    private void backToPreviousPane(Node node) {
+        MainPane.getChildren().clear();
+        MainPane.getChildren().add(node);
+
+        FadeTransition ft = new FadeTransition(Duration.millis(2500));
+        ft.setNode(node);
+        ft.setFromValue(2);
+        ft.setToValue(2);
+        ft.setCycleCount(1);
+        ft.setAutoReverse(false);
+        ft.play();
+    }
+    @FXML
+    private void backToAdminMatches (ActionEvent event) {
+        backToPreviousPane(back_to_admin_matches);
+    }
+    @FXML
+    private void awayRadioButtonAction (ActionEvent event) {
+        homeRadioButton.setSelected(false);
+        amount.setVisible(false);
+    }
+    @FXML
+    private void homeRadioButtonAction (ActionEvent event) {
+        awayRadioButton.setSelected(false);
+        amount.setVisible(true);
+    }
+    @FXML
+    private void addNewMatch (ActionEvent event) throws SQLException, ClassNotFoundException {
+        String type_match; int tickets_amount;
+        if(homeRadioButton.isSelected()){
+            type_match  = homeRadioButton.getText();
+            tickets_amount = Integer.parseInt(amount.getText());
+        }
+        else {
+            type_match = awayRadioButton.getText();
+            System.out.println(type_match);
+            tickets_amount = 0;
+        }
+        new DatabaseHandler().addNewMatches(new AdminMatchesWindowController.Match(Integer.parseInt(id.getText()), date.getText(),
+                time.getText(), type_match, opponent.getText(), tickets_amount));
+    }
+
+}
---
+++ src/main/java/com/example/arena_tickets_purchasing_system/Admin/AdminHomePageController.java
@@ -0,0 +1,106 @@
+package com.example.arena_tickets_purchasing_system.Admin;
+
+import com.example.arena_tickets_purchasing_system.WindowsOpener;
+import javafx.animation.FadeTransition;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.util.Duration;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AdminHomePageController {
+
+    public static String name;
+
+    @FXML
+    private Label AdminId;
+    @FXML
+    private AnchorPane MainPane;
+    @FXML
+    private Button Matches;
+
+    @FXML
+    private Button Settings;
+
+    @FXML
+    private Button Show;
+
+    @FXML
+    private Button Tickets;
+
+
+    AnchorPane match, news_page , settings_page, tickets_page;
+
+    @FXML
+    public void initialize() {
+        AdminId.setText(name);
+        FXMLLoader matches_loader = new FXMLLoader(); FXMLLoader tickets_loader = new FXMLLoader();
+        matches_loader.setLocation((getClass().getResource("admin_matches.fxml")));
+        tickets_loader.setLocation((getClass().getResource("tickets.fxml")));
+        try {
+            match = matches_loader.load();
+            tickets_page = tickets_loader.load();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @FXML
+    private void viewEditedMatches (ActionEvent some_event) {
+        setNewPane(match);
+    }
+
+    @FXML
+    private void viewAdminTickets (ActionEvent some_event) {
+        setNewPane(tickets_page);
+    }
+
+    @FXML
+    private void matchesAndTicketsSettings (ActionEvent some_event) {
+        setNewPane(settings_page);
+    }
+
+    @FXML
+    private void newsShow (ActionEvent some_event) {
+        setNewPane(news_page);
+    }
+    private void setNewPane(Node node) {
+        MainPane.getChildren().clear();
+        MainPane.getChildren().add(node);
+
+        FadeTransition ft = new FadeTransition(Duration.millis(1500));
+        ft.setNode(node);
+        ft.setFromValue(2);
+        ft.setToValue(2);
+        ft.setCycleCount(1);
+        ft.setAutoReverse(false);
+        ft.play();
+    }
+    private void closeStage() {
+        ((Stage) AdminId.getScene().getWindow()).close();
+    }
+    void loadWindow(String loc, String title) {
+        try {
+            Parent parent = FXMLLoader.load(getClass().getResource(loc));
+            Stage stage = new Stage(StageStyle.DECORATED);
+            stage.setTitle(title);
+            stage.setScene(new Scene(parent));
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
+
