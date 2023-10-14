d47ce8bb54f53f9b10213b55745e9c7dea0083bc
SanyaShbn <144464447+SanyaShbn@users.noreply.github.com>
2023-10-14 16:15:30 +0300

    Add files via upload

---
+++ src/main/java/com/example/arena_tickets_purchasing_system/Admin/AdminLoginController.java
@@ -0,0 +1,81 @@
+package com.example.arena_tickets_purchasing_system.Admin;
+
+import com.example.arena_tickets_purchasing_system.Admin.Admin;
+import com.example.arena_tickets_purchasing_system.DatabaseHandler;
+import com.example.arena_tickets_purchasing_system.WindowsOpener;
+import com.example.arena_tickets_purchasing_system.animations.Error_shaking;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextArea;
+import javafx.scene.text.Text;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class AdminLoginController {
+
+    @FXML
+    private TextArea LoginField;
+
+    @FXML
+    private PasswordField PasswordField;
+    @FXML
+    private Button SignInButton;
+
+    @FXML
+    private Button SignUpButton;
+
+    @FXML
+    void initialize() {
+        SignUpButton.setOnAction(event -> {
+            SignInButton.getScene().getWindow().hide();
+            WindowsOpener registration_window = new WindowsOpener("admin_registration.fxml");
+        });
+
+        SignInButton.setOnAction(event -> {
+            try {
+                signInAdmin();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+    private void signInAdmin() throws SQLException, ClassNotFoundException {
+        DatabaseHandler dbHandler = new DatabaseHandler();
+
+        String login = LoginField.getText();
+        String password = PasswordField.getText();
+
+        Admin admin = new Admin(login, password);
+
+        dbHandler.getAdmin(admin);
+        loginAdmin(login, password);
+    }
+    private void loginAdmin(String login, String password) throws SQLException {
+        DatabaseHandler dbHandler = new DatabaseHandler();
+        Admin admin = new Admin();
+        admin.setAdmin_login(login);
+        admin.setAdmin_password(password);
+        ResultSet result = dbHandler.getAdmin(admin);
+
+        int numb = 0;
+
+        while(result.next()){
+            numb++;
+        }
+
+        if(numb >= 1){
+            SignInButton.getScene().getWindow().hide();
+            WindowsOpener main_menu_window = new WindowsOpener("admin_home_page.fxml");
+        }
+        else{
+            Error_shaking login_and_password_shake = new Error_shaking(LoginField, PasswordField);
+            login_and_password_shake.executeAnimation();
+        }
+
+    }
+
+}
---
+++ src/main/java/com/example/arena_tickets_purchasing_system/Admin/AdminRegistrationController.java
@@ -0,0 +1,76 @@
+package com.example.arena_tickets_purchasing_system.Admin;
+
+import com.example.arena_tickets_purchasing_system.Admin.Admin;
+import com.example.arena_tickets_purchasing_system.DatabaseHandler;
+import com.example.arena_tickets_purchasing_system.WindowsOpener;
+import com.example.arena_tickets_purchasing_system.animations.Error_shaking;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextArea;
+import javafx.scene.text.Text;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class AdminRegistrationController {
+
+    @FXML
+    private TextArea LoginField;
+
+    @FXML
+    private TextArea PasswordField;
+
+    @FXML
+    private Button SignUpButton;
+
+    @FXML
+    void initialize() {
+
+        SignUpButton.setOnAction(event -> {
+            String login = LoginField.getText().trim();
+            String password = PasswordField.getText().trim();
+            if(!login.equals(" ") && !password.equals(" ")){
+                try {
+                    registerAdmin(login, password);
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                } catch (ClassNotFoundException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            else {
+                System.out.println("Ошибка регистрации!");
+            }
+        });
+    }
+
+    private void registerAdmin(String login, String password) throws SQLException, ClassNotFoundException {
+        DatabaseHandler dbHandler = new DatabaseHandler();
+        Admin admin = new Admin();
+        admin.setAdmin_login(login);
+        admin.setAdmin_password(password);
+        ResultSet result = dbHandler.getAdmin(admin);
+
+        int numb = 0;
+
+        while(result.next()){
+            numb++;
+        }
+
+        if(numb > 0){
+            Error_shaking login_and_password_shake = new Error_shaking(LoginField, PasswordField);
+            login_and_password_shake.executeAnimation();
+
+        }
+        else{
+            dbHandler.signUpAdmins(new Admin(login,password));
+            SignUpButton.getScene().getWindow().hide();
+            WindowsOpener open_window = new WindowsOpener("admin_login.fxml");
+
+        }
+
+    }
+
+}
+
