e915076c5dc5cd3f20ba72ee6c4baa96b00acfb6
SanyaShbn <144464447+SanyaShbn@users.noreply.github.com>
2023-10-15 14:03:29 +0300

    Update RegistrationController.java

--- src/main/java/com/example/arena_tickets_purchasing_system/User/RegistrationController.java
+++ src/main/java/com/example/arena_tickets_purchasing_system/User/RegistrationController.java
@@ -1,49 +1,26 @@
 package com.example.arena_tickets_purchasing_system.User;
 
 import com.example.arena_tickets_purchasing_system.DatabaseHandler;
-import com.example.arena_tickets_purchasing_system.User.User;
 import com.example.arena_tickets_purchasing_system.WindowsOpener;
 import com.example.arena_tickets_purchasing_system.animations.Error_shaking;
+import com.example.arena_tickets_purchasing_system.animations.NotificationShower;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextArea;
-import javafx.scene.text.Text;
-
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class RegistrationController {
 
-        @FXML
-        private TextArea LoginField;
+    @FXML
+    private TextArea LoginField;
 
-        @FXML
-        private TextArea PasswordField;
+    @FXML
+    private TextArea PasswordField;
 
-        @FXML
-        private Button SignUpButton;
-
-        @FXML
-    void initialize() {
-
-            SignUpButton.setOnAction(event -> {
-                String login = LoginField.getText().trim();
-                String password = PasswordField.getText().trim();
-                if(!login.equals(" ") && !password.equals(" ")){
-                    try {
-                        registerUser(login, password);
-                    } catch (SQLException e) {
-                        throw new RuntimeException(e);
-                    } catch (ClassNotFoundException e) {
-                        throw new RuntimeException(e);
-                    }
-                }
-                else {
-                    System.out.println("Ошибка регистрации!");
-                }
-            });
-        }
+    @FXML
+    private Button SignUpButton;
 
     private void registerUser(String login, String password) throws SQLException, ClassNotFoundException {
         DatabaseHandler dbHandler = new DatabaseHandler();
@@ -61,6 +38,7 @@ public class RegistrationController {
         if(numb > 0){
             Error_shaking login_and_password_shake = new Error_shaking(LoginField, PasswordField);
             login_and_password_shake.executeAnimation();
+            new NotificationShower().showSimpleError("Ошибка регистрации!", "Логин или пароль уже используются");
 
         }
         else{
@@ -72,5 +50,21 @@ public class RegistrationController {
 
     }
 
+    @FXML
+    private void registrationOfUser (ActionEvent some_event) {
+        String login = LoginField.getText().trim();
+        String password = PasswordField.getText().trim();
+        if(!LoginField.getText().isEmpty() && !PasswordField.getText().isEmpty()){
+            try {
+                registerUser(login, password);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        else {
+            new NotificationShower().showSimpleError("Ошибка регистрации!", "Введите логин и пароль");
+        }
+    }
 }
-
