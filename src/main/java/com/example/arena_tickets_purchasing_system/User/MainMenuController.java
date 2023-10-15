299964dcd879ab54d0f4005a4858d92e357d2360
SanyaShbn <144464447+SanyaShbn@users.noreply.github.com>
2023-10-15 12:32:41 +0300

    Update MainMenuController.java

--- src/main/java/com/example/arena_tickets_purchasing_system/User/MainMenuController.java
+++ src/main/java/com/example/arena_tickets_purchasing_system/User/MainMenuController.java
@@ -1,22 +1,17 @@
 package com.example.arena_tickets_purchasing_system.User;
 
-import com.example.arena_tickets_purchasing_system.Admin.AdminNewsController;
-import com.example.arena_tickets_purchasing_system.DatabaseHandler;
 import com.example.arena_tickets_purchasing_system.WindowsOpener;
+import javafx.animation.FadeTransition;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
+import javafx.scene.Node;
 import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.control.ScrollPane;
-import javafx.stage.Stage;
 
-import java.io.IOException;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import javafx.util.Duration;
 
-import static com.example.arena_tickets_purchasing_system.Constant.*;
+
 
 public class MainMenuController {
 
@@ -28,22 +23,39 @@ public class MainMenuController {
     private MenuItem MenuItem;
     @FXML
     private ScrollPane ScrollPane;
-
     @FXML
-    void initialize(){
-
-        ScrollPane.setPannable(true);
-        MenuItem news_item = new MenuItem("Новости");
-        MenuItem tickets_item = new MenuItem("Матчи");
-        MenuItem team_roster_item = new MenuItem("Команда");
-        MenuButton.getItems().addAll(news_item, tickets_item, team_roster_item);
-
-        tickets_item.setOnAction(event ->
-        {
-            MenuButton.getScene().getWindow().hide();
-            WindowsOpener matches_window = new WindowsOpener("matches.fxml");
-        });
-
+    private MenuItem MatchesItem;
+    @FXML
+    private MenuItem TeamItem;
+    @FXML
+    private MenuItem TicketsItem;
+    @FXML
+    private void goToNewPane(Node node) {
+        MainPane.getChildren().clear();
+        MainPane.getChildren().add(node);
+        FadeTransition ft = new FadeTransition(Duration.millis(2500));
+        ft.setNode(node);
+        ft.setFromValue(2);
+        ft.setToValue(2);
+        ft.setCycleCount(1);
+        ft.setAutoReverse(false);
+        ft.play();
+    }
+    @FXML
+    private void viewMatches (ActionEvent some_event) {
+        MainPane.getScene().getWindow().hide();
+        new WindowsOpener("matches.fxml");;
     }
 
+    @FXML
+    private void viewUserTickets (ActionEvent some_event) {
+        MainPane.getScene().getWindow().hide();
+        new WindowsOpener("user_tickets.fxml");;
+    }
+
+    @FXML
+    private void viewTeamRoster (ActionEvent some_event) {
+        MainPane.getScene().getWindow().hide();
+        new WindowsOpener("user_roster.fxml");;
+    }
 }
