b01eeff0a7bb1149cf3382d8d61d8ff7f84f9176
SanyaShbn <144464447+SanyaShbn@users.noreply.github.com>
2023-10-17 22:28:49 +0300

    Update WindowsOpener.java

--- src/main/java/com/example/arena_tickets_purchasing_system/WindowsOpener.java
+++ src/main/java/com/example/arena_tickets_purchasing_system/WindowsOpener.java
@@ -1,5 +1,7 @@
 package com.example.arena_tickets_purchasing_system;
 
+import com.example.arena_tickets_purchasing_system.Admin.AdminMatchesWindowController;
+import com.example.arena_tickets_purchasing_system.User.BookTicketsController;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -23,8 +25,26 @@ public class WindowsOpener {
         Stage stage = new Stage();
         stage.setScene(new Scene(root));
         stage.show();
-        if(!name.equals("registration.fxml") && !name.equals("admin_registration.fxml") && !name.equals("admin_login.fxml")) {
+        if(!name.equals("booking.fxml")) {
             stage.setMaximized(true);
         }
+
+    }
+    public WindowsOpener(String name, AdminMatchesWindowController.Match match){
+        file_name = name;
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation((getClass().getResource(file_name)));
+        BookTicketsController controller = new BookTicketsController();
+        controller.setMatch(match);
+        loader.setController(controller);
+        try {
+            loader.load();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        Parent root = loader.getRoot();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root));
+        stage.show();
     }
 }
