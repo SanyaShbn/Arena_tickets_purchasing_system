dd6e1cecdf6c16c1370d53ce3db4f6bcfc5d09b8
SanyaShbn <144464447+SanyaShbn@users.noreply.github.com>
2023-10-26 17:59:34 +0300

    Update AdminLoginController.java

--- src/main/java/com/example/arena_tickets_purchasing_system/Admin/AdminLoginController.java
+++ src/main/java/com/example/arena_tickets_purchasing_system/Admin/AdminLoginController.java
@@ -1,15 +1,22 @@
 package com.example.arena_tickets_purchasing_system.Admin;
 
+import com.example.arena_tickets_purchasing_system.ArenaTicketsPurchasingSystem;
 import com.example.arena_tickets_purchasing_system.DatabaseHandler;
 import com.example.arena_tickets_purchasing_system.WindowsOpener;
 import com.example.arena_tickets_purchasing_system.animations.Error_shaking;
 import com.example.arena_tickets_purchasing_system.animations.NotificationShower;
+import javafx.animation.FadeTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextArea;
+import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
 
+import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -25,6 +32,24 @@ public class AdminLoginController {
 
     @FXML
     private Button SignUpButton;
+
+    @FXML
+    private AnchorPane MainPane;
+
+    AnchorPane registration, admin_home_page;
+    @FXML
+    public void initialize() {
+        FXMLLoader registration_loader = new FXMLLoader();
+        FXMLLoader admin_home_page_loader = new FXMLLoader();
+        registration_loader.setLocation(ArenaTicketsPurchasingSystem.class.getResource("admin_registration.fxml"));
+        admin_home_page_loader.setLocation(ArenaTicketsPurchasingSystem.class.getResource("admin_home_page.fxml"));
+        try {
+            registration = registration_loader.load();
+            admin_home_page = admin_home_page_loader.load();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
     @FXML
     private void signInAdmin() throws SQLException, ClassNotFoundException {
         DatabaseHandler dbHandler = new DatabaseHandler();
@@ -51,8 +76,7 @@ public class AdminLoginController {
         }
 
         if(numb >= 1){
-            SignInButton.getScene().getWindow().hide();
-            WindowsOpener main_menu_window = new WindowsOpener("admin_home_page.fxml");
+            goToNewPane(admin_home_page);
         }
         else{
             Error_shaking login_and_password_shake = new Error_shaking(LoginField, PasswordField);
@@ -62,8 +86,28 @@ public class AdminLoginController {
 
     }
     @FXML
-    private void goToAdminRegistrationPage (ActionEvent some_event) {
-        SignInButton.getScene().getWindow().hide();
-        WindowsOpener registration_window = new WindowsOpener("admin_registration.fxml");
+    private void goToNewPane(Node node) {
+        MainPane.getChildren().clear();
+        MainPane.getChildren().add(node);
+        FadeTransition ft = new FadeTransition(Duration.millis(2500));
+        if(node == admin_home_page){
+            ft.setNode(node);
+            ft.setFromValue(0.1);
+            ft.setToValue(1);
+            ft.setCycleCount(1);
+            ft.setAutoReverse(false);
+        }
+        else {
+            ft.setNode(node);
+            ft.setFromValue(1);
+            ft.setToValue(1);
+            ft.setCycleCount(1);
+            ft.setAutoReverse(false);
+        }
+        ft.play();
+    }
+    @FXML
+    private void goToAdminRegistrationPage (ActionEvent some_event) {;
+       goToNewPane(registration);
     }
 }
