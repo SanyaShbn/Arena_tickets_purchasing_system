75ffab9e2a0ce3969b7eb3d917972cfa79e1f6a2
SanyaShbn <144464447+SanyaShbn@users.noreply.github.com>
2023-10-28 18:37:08 +0300

    Update AddTicketsController.java

--- src/main/java/com/example/arena_tickets_purchasing_system/Admin/AddTicketsController.java
+++ src/main/java/com/example/arena_tickets_purchasing_system/Admin/AddTicketsController.java
@@ -1,7 +1,8 @@
 package com.example.arena_tickets_purchasing_system.Admin;
 
+import com.example.arena_tickets_purchasing_system.ArenaTicketsPurchasingSystem;
 import com.example.arena_tickets_purchasing_system.DatabaseHandler;
-import com.example.arena_tickets_purchasing_system.WindowsOpener;
+import com.example.arena_tickets_purchasing_system.animations.NotificationShower;
 import javafx.animation.FadeTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -89,14 +90,14 @@ public class AddTicketsController {
                 }
             }
         }catch (SQLException e) {
-                throw new RuntimeException(e);
-            } catch (ClassNotFoundException e) {
-                throw new RuntimeException(e);
-            }
-        FXMLLoader add_matches_loader = new FXMLLoader();
-      admin_tickets_loader.setLocation(ArenaTicketsPurchasingSystem.class.getResource("tickets.fxml"));
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        FXMLLoader admin_tickets_loader = new FXMLLoader();
+        admin_tickets_loader.setLocation(ArenaTicketsPurchasingSystem.class.getResource("tickets.fxml"));
         try {
-            back_to_admin_tickets = add_matches_loader.load();
+            back_to_admin_tickets = admin_tickets_loader.load();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -122,10 +123,14 @@ public class AddTicketsController {
 
     @FXML
     private void addTickets (ActionEvent event) throws SQLException, ClassNotFoundException {
-        new DatabaseHandler().addNewAdminTickets(new AdminTicketsController.MatchTickets(Integer.parseInt(id.getText()), Integer.parseInt(amount.getText()),
-                Integer.parseInt(vip.getText()), Integer.parseInt(secA.getText()), Integer.parseInt(secB.getText()), Integer.parseInt(secC.getText()),
-                Integer.parseInt(secD.getText()), Integer.parseInt(secE.getText()), Integer.parseInt(secF.getText()), Integer.parseInt(secG.getText()),
-                Integer.parseInt(secH.getText()), Integer.parseInt(secI.getText())));
+        try {
+            new DatabaseHandler().addNewAdminTickets(new AdminTicketsController.MatchTickets(Integer.parseInt(id.getText()), Integer.parseInt(amount.getText()),
+                    Integer.parseInt(vip.getText()), Integer.parseInt(secA.getText()), Integer.parseInt(secB.getText()), Integer.parseInt(secC.getText()),
+                    Integer.parseInt(secD.getText()), Integer.parseInt(secE.getText()), Integer.parseInt(secF.getText()), Integer.parseInt(secG.getText()),
+                    Integer.parseInt(secH.getText()), Integer.parseInt(secI.getText())));
+        }catch(NumberFormatException e){
+            new NotificationShower().showWarning("Внимание!","Проверьте корректность ввода данных");
+        }
     }
 
 }
