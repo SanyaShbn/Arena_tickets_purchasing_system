4ddb1add2bcd5b6ce4d1bce8803a79c0406d2ce5
SanyaShbn <144464447+SanyaShbn@users.noreply.github.com>
2023-10-29 17:26:48 +0300

    Update MainMenuController.java

--- src/main/java/com/example/arena_tickets_purchasing_system/User/MainMenuController.java
+++ src/main/java/com/example/arena_tickets_purchasing_system/User/MainMenuController.java
@@ -1,16 +1,30 @@
 package com.example.arena_tickets_purchasing_system.User;
 
+import com.example.arena_tickets_purchasing_system.DatabaseHandler;
 import com.example.arena_tickets_purchasing_system.WindowsOpener;
 import javafx.animation.FadeTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.control.ScrollPane;
 
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.util.Duration;
 
+import java.io.IOException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static com.example.arena_tickets_purchasing_system.Constant.*;
+import static com.example.arena_tickets_purchasing_system.Constant.CONTESTS;
 
 
 public class MainMenuController {
@@ -29,6 +43,42 @@ public class MainMenuController {
     private MenuItem TeamItem;
     @FXML
     private MenuItem TicketsItem;
+
+    @FXML
+    void initialize() {
+        String select = "SELECT * FROM " + NEWS_TABLE;
+        PreparedStatement prStr = null;
+        try {
+
+            prStr = new DatabaseHandler().getDbConnection("news").prepareStatement(select);
+            ResultSet result = prStr.executeQuery();
+            double Y = 0;
+            while (result.next()) {
+                Circle date_circle = new Circle(30, Color.AZURE);
+                Text date_time = new Text(result.getString(PUBLISHING_DATE) + "\n    " + result.getString(PUBLISHING_TIME));
+                date_time.setStyle("-fx-font-size: 10;");
+                date_circle.setLayoutX(335);date_circle.setLayoutY(480 + Y);
+                date_time.setLayoutX(310);date_time.setLayoutY(484 + Y);
+                AnchorPane news_area = new AnchorPane();
+                news_area.setPrefWidth(590);
+                news_area.setPrefHeight(60);
+                news_area.setLayoutX(300);
+                news_area.setLayoutY(450 + Y);
+                Text news = new Text(result.getString(CONTESTS));
+                news.setFont(Font.font("Bodoni MT Black"));
+                news.setStyle("-fx-font-size: 16;");news_area.setStyle("-fx-background-color: #FFFFFF;");
+                news.setLayoutX(400);
+                news.setLayoutY(480 + Y);
+                Y = Y + 70;
+                MainPane.getChildren().addAll(news_area, date_circle, date_time, news);
+            }
+
+        } catch (SQLException e) {
+            new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
     @FXML
     private void goToNewPane(Node node) {
         MainPane.getChildren().clear();
@@ -44,18 +94,18 @@ public class MainMenuController {
     @FXML
     private void viewMatches (ActionEvent some_event) {
         MainPane.getScene().getWindow().hide();
-        new WindowsOpener("matches.fxml");;
+        new WindowsOpener("matches.fxml");
     }
 
     @FXML
     private void viewUserTickets (ActionEvent some_event) {
         MainPane.getScene().getWindow().hide();
-        new WindowsOpener("user_tickets.fxml");;
+        new WindowsOpener("user_tickets.fxml");
     }
 
     @FXML
     private void viewTeamRoster (ActionEvent some_event) {
         MainPane.getScene().getWindow().hide();
-        new WindowsOpener("user_roster.fxml");;
+        new WindowsOpener("user_roster.fxml");
     }
 }
