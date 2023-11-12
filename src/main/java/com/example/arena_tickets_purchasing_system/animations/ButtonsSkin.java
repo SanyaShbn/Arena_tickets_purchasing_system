7ddf565db79bb9bdbba1bef760a11d9f01ee89c9
SanyaShbn <144464447+SanyaShbn@users.noreply.github.com>
2023-11-12 14:52:27 +0300

    Update ButtonsSkin.java

--- src/main/java/com/example/arena_tickets_purchasing_system/animations/ButtonsSkin.java
+++ src/main/java/com/example/arena_tickets_purchasing_system/animations/ButtonsSkin.java
@@ -1,25 +1,11 @@
 package com.example.arena_tickets_purchasing_system.animations;
-
-import javafx.animation.FadeTransition;
 import javafx.scene.control.Button;
 import javafx.scene.control.skin.ButtonSkin;
-import javafx.util.Duration;
 public class ButtonsSkin extends ButtonSkin {
 
     public ButtonsSkin(Button button) {
         super(button);
-
-    final FadeTransition fadeIn = new FadeTransition(Duration.millis(100));
-    fadeIn.setNode(button);
-    fadeIn.setToValue(5);
-    button.setOnMouseEntered(e -> fadeIn.playFromStart());
-
-    final FadeTransition fadeOut = new FadeTransition(Duration.millis(100));
-    fadeOut.setNode(button);
-    fadeOut.setToValue(0.5);
-    button.setOnMouseExited(e -> fadeOut.playFromStart());
-
-    button.setOpacity(0.5);
-
+    button.setOnMouseEntered(event -> button.setStyle("-fx-background-radius: 20; -fx-background-color: #FFFFFF; -fx-border-radius: 20; -fx-border-color: #FF4500; -fx-text-fill: #FF4500"));
+    button.setOnMouseExited(event -> button.setStyle("-fx-background-radius: 20; -fx-background-color: #FF4500; -fx-border-radius: 20; -fx-border-color: #FF4500; -fx-text-fill: #000000"));
     }
 }
