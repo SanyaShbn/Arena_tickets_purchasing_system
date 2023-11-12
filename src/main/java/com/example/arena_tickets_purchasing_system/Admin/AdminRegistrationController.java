326a52c65070dec1020df3946c91aa8cf8636641
SanyaShbn <144464447+SanyaShbn@users.noreply.github.com>
2023-11-12 14:55:17 +0300

    Update AdminRegistrationController.java

--- src/main/java/com/example/arena_tickets_purchasing_system/Admin/AdminRegistrationController.java
+++ src/main/java/com/example/arena_tickets_purchasing_system/Admin/AdminRegistrationController.java
@@ -1,14 +1,19 @@
 package com.example.arena_tickets_purchasing_system.Admin;
 
+import com.example.arena_tickets_purchasing_system.ArenaTicketsPurchasingSystem;
 import com.example.arena_tickets_purchasing_system.DatabaseHandler;
 import com.example.arena_tickets_purchasing_system.WindowsOpener;
 import com.example.arena_tickets_purchasing_system.animations.Error_shaking;
 import com.example.arena_tickets_purchasing_system.animations.NotificationShower;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.TextArea;
+import javafx.scene.layout.AnchorPane;
 
+import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -23,6 +28,22 @@ public class AdminRegistrationController {
     @FXML
     private Button SignUpButton;
 
+    @FXML
+    private AnchorPane MainPane;
+
+    @FXML
+    private Hyperlink ExitLink;
+    AnchorPane new_pane;
+
+    @FXML
+    public void initialize() {
+        SignUpButton.setOnMouseEntered(event -> {
+            SignUpButton.setStyle("-fx-background-color: #FFFFFF; -fx-border-color: #0000FF; -fx-text-fill: #0000FF");
+        });
+        SignUpButton.setOnMouseExited(event -> {
+            SignUpButton.setStyle("-fx-background-color: #0000FF; -fx-border-color: #0000FF; -fx-text-fill: #FFFFFF");
+        });
+    }
     private void registerAdmin(String login, String password) throws SQLException, ClassNotFoundException {
         DatabaseHandler dbHandler = new DatabaseHandler();
         Admin admin = new Admin();
@@ -40,6 +61,8 @@ public class AdminRegistrationController {
             Error_shaking login_and_password_shake = new Error_shaking(LoginField, PasswordField);
             login_and_password_shake.executeAnimation();
             new NotificationShower().showSimpleError("Ошибка регистрации!", "Логин или пароль уже используются");
+            LoginField.clear();
+            PasswordField.clear();
         }
         else{
             dbHandler.signUpAdmins(new Admin(login,password));
@@ -65,5 +88,17 @@ public class AdminRegistrationController {
         else {
             new NotificationShower().showSimpleError("Ошибка регистрации!", "Введите логин и пароль");
         }
+    }
+    @FXML
+    private void goToAdminLoginPage (ActionEvent some_event) {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(ArenaTicketsPurchasingSystem.class.getResource("admin_login.fxml"));
+        MainPane.getChildren().clear();
+        try {
+            new_pane = loader.load();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        MainPane.getChildren().add(new_pane);
     }
 }
