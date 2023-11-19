aec80fa43926f99ab4e573f1a12cf940ccee7a4d
SanyaShbn <144464447+SanyaShbn@users.noreply.github.com>
2023-11-19 09:35:45 +0300

    Update Admin.java

--- src/main/java/com/example/arena_tickets_purchasing_system/Admin/Admin.java
+++ src/main/java/com/example/arena_tickets_purchasing_system/Admin/Admin.java
@@ -1,29 +1,19 @@
 package com.example.arena_tickets_purchasing_system.Admin;
 
 public class Admin {
+    private static Admin instance;
     public String admin_login;
     public String admin_password;
-
-    public Admin(String admin_login, String admin_password ){
-        this.admin_login = admin_login;
-        this.admin_password = admin_password;
-
+    private Admin(String login, String password){
+        admin_login = login;
+        admin_password = password;
     }
-    public Admin() {
-
-    }
-    public void setAdmin_login(String user_login) {
-        this.admin_login = user_login;
-    }
-    public String getAdmin_login(){
-        return admin_login;
+    public static Admin getInstance(String login, String password){
+        if(instance == null) {
+            instance = new Admin(login, password);
+        }
+        return instance;
     }
 
-    public void setAdmin_password(String admin_password ) {
-        this.admin_password  = admin_password ;
-    }
-    public String getAdmin_password(){
-        return admin_password ;
-    }
 
 }
